92429e3663a33a663303e7f62f03d54ccedf21bd
beginor <beginor@qq.com>
2019-08-15 17:53:57 +0800

    chmod -x

--- src/initdb-postgis.sh
+++ src/initdb-postgis.sh
@@ -13,11 +13,11 @@ EOSQL
 
 # Load PostGIS into both template_database and $POSTGRES_DB
 for DB in template_postgis "$POSTGRES_DB"; do
-    echo "Loading PostGIS extensions into $DB"
-    "${psql[@]}" --dbname="$DB" <<-'EOSQL'
-        CREATE EXTENSION IF NOT EXISTS postgis;
-        CREATE EXTENSION IF NOT EXISTS postgis_topology;
-        CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-        CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+  echo "Loading PostGIS extensions into $DB"
+  "${psql[@]}" --dbname="$DB" <<-'EOSQL'
+    CREATE EXTENSION IF NOT EXISTS postgis;
+    CREATE EXTENSION IF NOT EXISTS postgis_topology;
+    CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+    CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
 done
