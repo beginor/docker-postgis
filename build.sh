1b47125f934aaa7635b6a3ddc3c97e8be9216be7
beginor <beginor@qq.com>
2025-04-10 21:35:55 +0800

    update tag version

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 docker buildx build \
   --platform linux/amd64 \
   --platform linux/arm64 \
-  -t beginor/postgis:16-3.4 \
-  -t beginor/postgis:16-3.4.$(date +"%Y%m%d%H%M%S") \
+  -t beginor/postgis:17-3.5 \
+  -t beginor/postgis:17-3.5.$(date +"%Y%m%d%H%M%S") \
   --push \
   .
